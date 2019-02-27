9c960a3f284687768d367b68131f5e4625a7c656
denmaseno <agus@senomas.com>
2019-02-27 17:07:04 +0700

    fix tcp6

--- index.js
+++ index.js
@@ -133,9 +133,9 @@ const spawnSync = async (cmd, options = {}) => {
 const checkPorts = ports => {
   const lports = [];
   netstat(
-    { sync: true, filter: { protocol: "tcp", state: "LISTENING" } },
+    { sync: true, filter: { state: "LISTENING" } },
     data => {
-      if (data.local.port) {
+      if (data.local.port && (protocol === "tcp" || protocol === "tcp6")) {
         lports.push(data.local.port);
       }
     }
